de16bcd91d2c9c89157cf45003865ae34ca91b09
darylmathison <daryl.mathison@gmail.com>
2022-01-16 11:36:12 -0500

    Added Barometer

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,53 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { Barometer } from 'expo-sensors';
 
 export default class Weather extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+            pressure: 0,
+            relativeAltitude: 0
+        };
+
+        this._subscription = Barometer.addListener(barometerData => {
+            this.setState({
+                pressure: barometerData.pressure,
+                relativeAltitude: barometerData.relativeAltitude
+            });
+        });
+
+        this.styles = StyleSheet.create({
+            buttonContainer: {
+                flexDirection: 'row',
+                alignItems: 'stretch',
+                marginTop: 15,
+            },
+            button: {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#eee',
+                padding: 10,
+            },
+            sensor: {
+                marginTop: 45,
+                paddingHorizontal: 10,
+            },
+        });
+
     }
+
     render() {
         return (
-            <View>
-                <Text>It is {this.state.date.toLocaleTimeString()}.</Text>
+            <View style={this.styles.sensor}>
+                <Text>Barometer:</Text>
+                <Text>Pressure: {this.state.pressure * 100} Pa</Text>
+                <Text>
+                    Relative Altitude:{' '}
+                    {Platform.OS === 'ios' ? `${this.state.relativeAltitude} m` : `Only available on iOS`}
+                </Text>
             </View>
-        );
+        )
     }
 }
